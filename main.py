a08e1fedee13b08309003bbd0f159a008d458252
FoggyCaligo <foggycaligo@gmail.com>
2023-07-15 17:42:39 +0900

    fin making f_getClosePriceData

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
-import sys
-from PyQt5.
+from pykiwoom.kiwoom import *
+
+kiwoom=Kiwoom()
+kiwoom.CommConnect(block=True)
+
+account_num = kiwoom.GetLoginInfo("ACCOUNT_CNT") #전체 계좌 수
+accounts=kiwoom.GetLoginInfo("ACCNO")#전체 계좌 리스트
+
+user_id = kiwoom.GetLoginInfo("USER_ID")
+user_name = kiwoom.GetLoginInfo("USER_NAME")
+keyboard = kiwoom.GetLoginInfo("KEY_BSECGB")#키보드보안 해지여부
+firewall = kiwoom.GetLoginInfo("FIREW_SECGB")
+
+print(account_num)
+print(accounts)
+print(user_id)
+print(user_name)
+print(keyboard)
+print(firewall)
+
+
